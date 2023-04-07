27c9fcbd1324b74601ad94421871e67e7b3af123
ImDebabrata <debabratakgt@hotmail.com>
2023-04-08 00:16:35 +0530

    post viewer

--- social-connect/src/components/postviewer/PostViewer.tsx
+++ social-connect/src/components/postviewer/PostViewer.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import style from "./postviewer.module.scss";
 import { MdOutlineFavoriteBorder, MdFavorite, MdComment } from "react-icons/md";
+import { SlOptionsVertical } from "react-icons/sl";
+import { RiCloseFill } from "react-icons/ri";
+import { FaUserCircle } from "react-icons/fa";
 
 const PostViewer = () => {
   return (
     <div className={style.container}>
-      <div className="options"></div>
+      <div className={style.options}>
+        {/* <SlOptionsVertical /> */}
+        <RiCloseFill />
+      </div>
       <div className={style.heading}>
-        <div className={style.avatar}></div>
+        <div className={style.avatar}>
+          {/* <FaUserCircle /> */}
+          <img
+            src="https://avatars.githubusercontent.com/u/96492019?v=4"
+            alt="avatar"
+          />
+        </div>
         <div className={style.name}>Debabrata Datta</div>
         <div className={style.email}>debabratakgt@hotmail.com</div>
       </div>
@@ -21,7 +33,12 @@ const PostViewer = () => {
         </p>
       </div>
       {/* conditional rendering when image is available */}
-      <div className={style.post_image}></div>
+      <div className={style.post_image}>
+        <img
+          src="https://i2.wp.com/digital-photography-school.com/wp-content/uploads/2014/10/backgrounds-portraits-6.jpg?resize=90%2C60&ssl=1"
+          alt="post image"
+        />
+      </div>
       <div className={style.like_comment}>
         <div>
           <span>Like</span>
